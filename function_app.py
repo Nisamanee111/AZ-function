48e90411cfe00eb8bfe9f98c95ac860e4ca78a1d
Nisamanee <sinisamanee@central.co.th>
2024-01-29 17:16:28 +0700

    func

---
+++ function_app.py
@@ -0,0 +1,51 @@
+import logging
+import azure.functions as func
+import requests
+from pymongo import MongoClient
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 */30 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger_temp(myTimer: func.TimerRequest) -> None:
+    client = MongoClient('mongodb+srv://nisamanee:passw0rd!@ct-pj-iot.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000')
+    db = client['Project']
+    collection = db['Temp']
+    cursor = collection.find().sort("_id", -1).limit(1)
+    last_document = next(cursor, None)
+    Temp = last_document.get("Status")
+
+    if Temp >= 25:
+        message = "status", " Temp over 27"
+        send_line_notify(message)
+    else:
+        logging.info("Not Alert")
+
+@app.schedule(schedule="0 */30 * * * *", arg_name="myTimer1", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger_humi(myTimer1: func.TimerRequest) -> None:
+    client = MongoClient('mongodb+srv://nisamanee:passw0rd!@ct-pj-iot.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000')
+    db = client['Project']
+    collection = db['Humidity']
+    cursor = collection.find().sort("_id", -1).limit(1)
+    last_document = next(cursor, None)
+    Temp = last_document.get("Status")
+
+    if Temp >= 40:
+        message = "status", "Humidity over 40"
+        send_line_notify(message)
+    else:
+        logging.info("Not Alert")
+
+def send_line_notify(message):
+    token = 'tWL6dYjuQRCk3S88MdjAeLAQI3yRH6Nkj8QwMtJrddC'
+    url = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {token}'}
+    data = {'message': message}
+    response = requests.post(url, headers=headers, data=data)
+    logging.info(response.text)
+
+    #if myTimer.past_due:
+        #logging.info('The timer is past due!')
+
+   # logging.info('Python timer trigger function executed.')
